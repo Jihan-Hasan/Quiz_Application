f4d90927d5d3af6df1e4af68e7bab277c1ce6ad0
STK <mailto:jihanofficailstk@gmail.com>
2022-07-13 14:40:12 +0600

    Second push

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+
+import '../style/App.css';
+import Layout from './Layout';
+import Home from './pages/Home';
+
+function App() {
+  return (
+      <Layout>
+        <Home/>
+      </Layout>
+  );
+}
+
+export default App;
---
+++ src/components/Video.js
@@ -0,0 +1,20 @@
+import image from "../asset/images/3.jpg"
+import classes from "../style/Videos.module.css"
+
+export default function Video (){
+    return(
+        <a href="quiz.html"
+            ><div className={classes.video}>
+              <img src={image} alt="Thubnail" />
+              <p>#23 React Hooks Bangla - React useReducer hook Bangla</p>
+              <div className={classes.qmeta}>
+                <p>10 Questions</p>
+                <p>Score : Not taken yet</p>
+              </div>
+            </div>
+          </a>
+
+
+        
+    );
+}
